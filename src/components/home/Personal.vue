0daa0791b97e1da8c4cc1ce5dd9a233c18e35c56
zhengjiantao <zhengjiantao@suancloud.cn>
2019-04-18 14:55:07 +0800

    zjt

--- src/components/home/Personal.vue
+++ src/components/home/Personal.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="Personal">
-      Personal
+  <div class="personal">
+    <div class="info">
+         <span><img src="../../assets/images/person.png" height="28" width="28" alt="author-img"></span>
+         <span>user</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Personal',
-  data () {
-    return {
-    }
+  name: "Personal",
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style scoped>
+.personal{
+    display: flex;
+    flex-direction: column;
+}
+.info{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 2rem;
+    line-height: 2rem;
+    background-color: #fff;
+    margin: 10px auto;
+    padding: 0.3rem
+}
+.info span{
+}
 </style>
